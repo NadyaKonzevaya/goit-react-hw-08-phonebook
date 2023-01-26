9c14e6e2e7220b6617f76a5ef8019f4be65aacb8
NadyaKonzevaya <borisova.nadya@gmail.com>
2023-01-26 16:31:38 +0300

    Create Filter.styled.jsx

---
+++ src/components/Filter/Filter.styled.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+    margin: 0 auto;
+`;
+
+export const InputName = styled.input`
+    border: 1px solid gray;
+    border-radius: 5px;
+    outline: none;
+    &:hover, &:focus {
+        border: 5px solid #81b9e7;
+    }
+`;
