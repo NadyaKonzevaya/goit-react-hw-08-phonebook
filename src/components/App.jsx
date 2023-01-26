26a2c1988ada5d6e97e621a63073c36d4a2a2970
NadyaKonzevaya <borisova.nadya@gmail.com>
2023-01-26 16:11:17 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+// import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import PhonebookForm from "./PhonebookForm";
+import ContactList from "./ContactList";
+import Filter from "./Filter";
+import { Container } from "./App.styled";
+
+export function App() {
+    return (
+    <Container>
+      <h1>Phonebook</h1>
+      <PhonebookForm  />
+      <h2>Contacts</h2>
+      <Filter  />
+      <ContactList />
+    </Container>
+    );
+  
 };
+
+App.propTypes = {
+  contacts: PropTypes.array,
+  filter: PropTypes.string
+}
