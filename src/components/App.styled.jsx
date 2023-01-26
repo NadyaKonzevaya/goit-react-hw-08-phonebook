85d1a8ac7fe4ad4c9e6bdd777d14ef82a787fa2d
NadyaKonzevaya <borisova.nadya@gmail.com>
2023-01-26 16:31:50 +0300

    Create App.styled.jsx

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: auto;
+  max-width: 500px;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: rgb(244, 165, 247);
+  box-shadow: 10px 10px 26px 0px rgba(0, 0, 0, 0.5);
+  text-align: center;
+`;
