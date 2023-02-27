39b31a191303fb814a0fc2c29b47b627c9409d57
NadyaKonzevaya <borisova.nadya@gmail.com>
2023-02-27 20:30:30 +0300

    remove AxiosError

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,4 +1,5 @@
-import axios, { AxiosError } from 'axios';
+// import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 
